b55513ebba97784fa440941fc6fa09d424760f27
zxian <542883066@qq.com>
2019-10-21 14:09:16 +0800

    Merge branch 'master' of https://github.com/zxian/Scooter
    # Conflicts:
    #	app/src/main/java/com/xian/scooter/net/HttpURL.java

---
+++ app/src/main/java/com/xian/scooter/module/stores/SetUpFeedbackActivity.java
@@ -0,0 +1,20 @@
+package com.xian.scooter.module.stores;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.xian.scooter.R;
+import com.xian.scooter.base.BaseActivity;
+
+public class SetUpFeedbackActivity extends BaseActivity {
+
+    @Override
+    protected int getLayoutResourceId() {
+        return R.layout.activity_set_up_feedback;
+    }
+
+    @Override
+    protected void init() {
+
+    }
+}
